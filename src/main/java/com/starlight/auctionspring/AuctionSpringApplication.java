a963f41981603911ebcf7e3df05faa96b1e84ad4
Andrei Vasileuski <vasileuski.andrei@gmail.com>
2022-03-15 11:47:08 +0300

    add new empty project

---
+++ src/main/java/com/starlight/auctionspring/AuctionSpringApplication.java
@@ -0,0 +1,13 @@
+package com.starlight.auctionspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starlight/auctionspring/AuctionSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starlight.auctionspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
