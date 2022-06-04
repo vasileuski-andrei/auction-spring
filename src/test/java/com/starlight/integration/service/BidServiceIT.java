f89fd4b8afc4e05263e4cb502c6d30fd23a9182f
Andrei Vasileuski <vasileuski.andrei@gmail.com>
2022-06-04 20:49:26 +0300

    BidServiceIT tests

--- src/test/java/com/starlight/integration/service/BidServiceIT.java
+++ src/test/java/com/starlight/integration/service/BidServiceIT.java
@@ -1,19 +1,44 @@
 package com.starlight.integration.service;
 
+import com.starlight.dto.BidDto;
+import com.starlight.exception.ValidationException;
 import com.starlight.integration.IntegrationTestBase;
 import com.starlight.service.BidService;
+import com.starlight.service.LotService;
+import com.starlight.util.LotCountdown;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BidServiceIT extends IntegrationTestBase {
 
     private static final Long TEST_LOT_ID = 1L;
+    private static final String TEST_USER = "TestUser2";
+    private static final Integer TEST_USER_BID = 50;
+    private static final Integer TEST_USER_BID_NEGATIVE = -50;
+    private static final Integer TEST_START_BID = 1;
+    private static BidDto bidDto;
 
     @Autowired
     private BidService bidService;
+    @Autowired
+    private LotService lotService;
+
+    @BeforeAll
+    static void init() {
+        bidDto = BidDto.builder()
+                .lotName("TestLot")
+                .lotId(TEST_LOT_ID)
+                .username(TEST_USER)
+                .startBid(TEST_START_BID)
+                .build();
+    }
 
     @Test
     void listOfLotBidsIsPresentTest() {
@@ -22,7 +47,34 @@ public class BidServiceIT extends IntegrationTestBase {
 
         assertAll(() -> assertThat(bidDtoList).isNotEmpty(),
                   () -> assertThat(userBid).isNotNull());
+    }
 
+    @Test
+    void successfullyAddBidTest() throws NoSuchFieldException, IllegalAccessException {
+        startLotCountdownForLot();
+        bidDto.setUserBid(TEST_USER_BID);
+
+        assertDoesNotThrow(() -> bidService.create(bidDto));
+    }
+
+    @Test
+    void throwExceptionIfValidationIsNotPassedTest() throws NoSuchFieldException, IllegalAccessException {
+        startLotCountdownForLot();
+        bidDto.setUserBid(TEST_USER_BID_NEGATIVE);
+
+        assertThrows(ValidationException.class, () -> bidService.create(bidDto));
+    }
+
+    private void startLotCountdownForLot() throws NoSuchFieldException, IllegalAccessException {
+        var declaredField = lotService.getClass().getDeclaredField("lotCountdown");
+        declaredField.setAccessible(true);
+        Map<Long, LotCountdown> lotCountdown = (Map)declaredField.get(lotService);
+        lotCountdown.put(TEST_LOT_ID, new LotCountdown());
+    }
+
+    @AfterAll
+    static void destroy() {
+        bidDto = null;
     }
 
 }
--- src/test/resources/sql/data.sql
+++ src/test/resources/sql/data.sql
@@ -1,6 +1,7 @@
 INSERT INTO users(id, username, birth_date, email, password, role, user_status)
 VALUES(1, 'admin', '2022-03-11', 'admin@admin.com', '$2a$12$o3lI0qeOG9h.B7RFkoUDLOY9VJa/Ih5Hl1Aa34.ATNb1tTrogOCnu', 'USER', 'ACTIVE'),
-      (2, 'TestUser', '2022-03-13', 'test@test.com', '$2a$12$o3lI0qeOG9h.B7RFkoUDLOY9VJa/Ih5Hl1Aa34.ATNb1tTrogOCnu', 'USER', 'ACTIVE');
+      (2, 'TestUser', '2022-03-13', 'test@test.com', '$2a$12$o3lI0qeOG9h.B7RFkoUDLOY9VJa/Ih5Hl1Aa34.ATNb1tTrogOCnu', 'USER', 'ACTIVE'),
+      (3, 'TestUser2', '2022-03-13', 'test2@test.com', '$2a$12$o3lI0qeOG9h.B7RFkoUDLOY9VJa/Ih5Hl1Aa34.ATNb1tTrogOCnu', 'USER', 'ACTIVE');
 SELECT SETVAL('users_id_seq', (SELECT MAX(id) FROM users));
 
 INSERT INTO lot(id, lot_name, lot_owner, start_bid, status_id)
@@ -8,5 +9,5 @@ VALUES(1, 'TestLot', 'admin', 1, 1);
 SELECT SETVAL('lot_id_seq', (SELECT MAX(id) FROM lot));
 
 INSERT INTO bid(id, lot_name, lot_id, username, user_bid)
-VALUES(1, 'TestLot', 1, 'TestUser', 100);
+VALUES(1, 'TestLot', 1, 'TestUser', 10);
 SELECT SETVAL('bid_id_seq', (SELECT MAX(id) FROM bid));
